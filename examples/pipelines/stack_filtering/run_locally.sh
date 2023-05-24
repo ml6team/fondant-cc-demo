1443a9b44bf10516cddf52c6f6d4af482eddfbe6
Niels Rogge <nielsrogge@Nielss-MacBook-Pro.local>
2023-05-24 16:29:02 +0200

    More improvements

--- examples/pipelines/stack_filtering/run_locally.sh
+++
@@ -1,9 +0,0 @@
-export BASE_PATH="/home/philippe/Scripts/fondant-cc-demo/local_artifacts"
-export METADATA='{"run_id":"test-demo", "base_path":"/home/philippe/Scripts/fondant-cc-demo/local_artifacts"}'
-
-echo $METADATA
-cd components/load_from_hub_stack/src
-python main.py \
- --metadata "$METADATA" \
- --output_manifest_path ${BASE_PATH}/load_from_hub_manifest.txt \
- --dataset_name "ml6team/the-stack-smol-python"
