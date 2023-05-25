820df27172c488407a0e40818599a6e081793a03
Philippe Moussalli <philippe.moussalli95@gmail.com>
2023-05-25 09:29:54 +0200

    add commeent filtering component

--- examples/pipelines/stack_filtering/run_locally.sh
+++ examples/pipelines/stack_filtering/run_locally.sh
@@ -1,7 +1,7 @@
 export BASE_PATH="/home/philippe/Scripts/fondant-cc-demo/local_artifacts"
 export METADATA='{"run_id":"test-demo", "base_path":"/home/philippe/Scripts/fondant-cc-demo/local_artifacts"}'
 
-echo $METADATA
+
 cd components/load_from_hub_stack/src
 python main.py \
  --metadata "$METADATA" \
@@ -17,3 +17,13 @@ python main.py \
  --avg_line_length_threshold 10 \
  --max_line_length_threshold 100 \
  --alphanum_fraction_threshold 0.25
+
+cd ../..
+cd comments_filtering/src
+python main.py \
+ --metadata "$METADATA" \
+ --input_manifest_path ${BASE_PATH}/load_from_hub_manifest.txt \
+ --output_manifest_path ${BASE_PATH}/comments_filtering_manifest.txt \
+ --min_comments_ratio 0.1 \
+ --max_comments_ratio 0.9
+
